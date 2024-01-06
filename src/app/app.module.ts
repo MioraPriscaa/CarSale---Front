13675479d171aded6e1759923416f85a46643980
Seesiz <108148026+Seesiz@users.noreply.github.com>
2024-01-06 20:26:44 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 @NgModule({
   declarations: [AppComponent, MainComponent, AnnonceComponent, OneComponent],
   imports: [
@@ -28,6 +29,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatButtonModule,
     MatDividerModule,
     MatIconModule,
+    MatChipsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
