a67df1caa5a2f05ba424ad03614ae6cc68395c52
Seesiz <108148026+Seesiz@users.noreply.github.com>
2024-01-20 13:12:54 +0300

    message

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { ErrorService } from '../Service/error.service';
 import { GenericService } from '../Service/generic.service';
 import axios from 'axios';
@@ -8,14 +16,14 @@ import axios from 'axios';
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css'],
 })
-export class MessageComponent {
+export class MessageComponent implements AfterViewInit {
+  @ViewChild('message') private messageZone: ElementRef | undefined;
   @Output() showMessage: EventEmitter<any> = new EventEmitter<any>();
   @Output() closeMessage: EventEmitter<void> = new EventEmitter<void>();
   me: number = 0;
   messages: any[] = [];
   error: any = {};
-  newMessage:String = "";
-  
+  newMessage: String = '';
 
   styleMe: string =
     'max-width: 80%; border-radius: 12px; border-top-right-radius: 0px; background-color: var(--secondary); float: right;';
@@ -28,21 +36,42 @@ export class MessageComponent {
     const me = localStorage.getItem('CarsalidPersonne');
     if (me != null) {
       this.me = Number.parseInt(me);
-      this.getAllMessages(this.data.idPersonne);
+      await this.getAllMessages(this.data.idPersonne);
+      setInterval(async () => {
+        await this.getAllMessages(this.data.idPersonne);
+      }, 700);
     }
   }
-  
-  
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.showScroll();
+    }, 500);
+  }
+
+  showScroll() {
+    try {
+      if (this.messageZone) {
+        this.messageZone.nativeElement.scrollTop =
+          this.messageZone.nativeElement.scrollHeight;
+      }
+    } catch (err) {
+      alert(err);
+    }
+  }
+
   constructor(
     private ErrorService: ErrorService,
     private genericService: GenericService
   ) {
     this.ErrorService.data$.subscribe((value) => (this.error = value));
   }
-  
-  async getAllMessages(idContact:number){
+
+  async getAllMessages(idContact: number) {
     try {
-      const response = await this.genericService.getAll('messages/entre?idUser='+this.me+'&&idContact='+idContact);
+      const response = await this.genericService.getAll(
+        'messages/entre?idUser=' + this.me + '&&idContact=' + idContact
+      );
       this.messages = response.contacts;
     } catch (error) {
       this.error = {
@@ -53,22 +82,25 @@ export class MessageComponent {
       return;
     }
   }
-  
-  async sendMessage(){
+
+  async sendMessage() {
     try {
       const message = {
-        dateEnvoye:this.dateNow(),
-        sender:{
-          idPersonne:this.me
+        dateEnvoye: this.dateNow(),
+        sender: {
+          idPersonne: this.me,
         },
-        receiver:{
-          idPersonne:this.data.idPersonne
+        receiver: {
+          idPersonne: this.data.idPersonne,
         },
-        content:this.newMessage
+        content: this.newMessage,
       };
-      const response = await this.genericService.insert('messages',message);
-      await this.getAllMessages(this.data.idPersonne);
-      this.newMessage = "";
+      const response = await this.genericService.insert('messages', message);
+      this.messages.push(message);
+      this.newMessage = '';
+      setTimeout(() => {
+        this.showScroll();
+      }, 500);
     } catch (error) {
       this.error = {
         statut: true,
@@ -78,13 +110,7 @@ export class MessageComponent {
       return;
     }
   }
-  
-  changeNewMessage(event:Event){
-    const newMessageInput = (event.target as HTMLInputElement).value;
-    this.newMessage = newMessageInput;
-  }
-  
-  
+
   dateNow() {
     var now = new Date();
     var resp = now.getFullYear() + '-';
@@ -104,8 +130,4 @@ export class MessageComponent {
     else resp += now.getSeconds();
     return resp;
   }
-  
-  
-  
-  
 }
