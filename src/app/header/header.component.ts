5d81ed2aad732d18cf74b4cfe0895cc041f84d0c
DydiRah <124720797+DydiRah@users.noreply.github.com>
2024-01-20 09:50:00 +0300

    2024-01--20

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, HostListener, Output } from '@angular/core';
 import { ErrorService } from '../Service/error.service';
 import { ConnectionService } from '../Service/connection.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { GenericService } from './../Service/generic.service';
 
 @Component({
   selector: 'app-header',
@@ -15,17 +16,60 @@ export class HeaderComponent {
 
   isConnected: boolean = false;
   error: any = {};
+  data:any[] = [];
+  contacts:any[] = [];
+  newMessage = 0;
+  
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private ErrorService: ErrorService,
-    private ConnectionService: ConnectionService
+    private ConnectionService: ConnectionService,
+    private genericService: GenericService
   ) {
     this.ErrorService.data$.subscribe((value) => (this.error = value));
     this.ConnectionService.data$.subscribe(
       (value) => (this.isConnected = value.statut)
     );
+  }
+  
+  async ngOnInit() {
+    await this.getAllContact();
+  }
+  
+  async getAllContact(){
+    const idUser = localStorage.getItem("CarsalidPersonne");
+    try {
+      const response = await this.genericService.getAll('messages/contacts?idUser=' + idUser);
+      this.data = response.contacts;
+      this.newMessage = 0;
+      this.data.map((element)=>{
+        var personne:any = {};
+        var lastMessage = {};
+        if(element.sender.idPersonne == idUser) {
+          personne = element.receiver;
+          lastMessage = 'Vous: ' + element.content;
+          element.etat = 10;
+        }else{
+          personne = element.sender;
+          lastMessage = element.sender.prenom + ': ' + element.content;
+        }
+        if(element.etat == 0)this.newMessage += 1;
+        this.contacts.push({
+          personne:personne,
+          lastMessage:lastMessage,
+          etat:element.etat
+        });
+      });
+    } catch (error) {
+      this.error = {
+        statut: true,
+        message: error,
+      };
+      this.ErrorService.updateData(this.error);
+      return;
+    }
   }
 
   animationFrameId: number | null = null;
